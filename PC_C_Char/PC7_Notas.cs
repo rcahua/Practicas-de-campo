87263bd3bb1ca4bfc4164d73b12478699ff36a5a
Raul Oscar Cahua Escate <n00057365@upn.pe>
2026-05-10 17:22:36 -0500

    PC71_Entregado

---
+++ PC_C_Char/PC7_Notas.cs
@@ -0,0 +1,95 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double suma = 0;
+        int aprobados = 0;
+        double mayor = 0;
+        double menor = 20;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            double nota = LeerNota("Ingrese nota:");
+
+            suma = suma + nota;
+
+            if (EsAprobado(nota))
+            {
+                aprobados++;
+            }
+
+            if (nota > mayor)
+            {
+                mayor = nota;
+            }
+
+            if (nota < menor)
+            {
+                menor = nota;
+            }
+
+            Console.WriteLine(ClasificarNota(nota));
+        }
+
+        double prom = CalcularPromedio(suma, 3);
+
+        MostrarEstadisticas(prom, mayor, menor, aprobados, 3);
+
+        Console.ReadKey();
+    }
+
+    static double LeerNota(string mensaje)
+    {
+        double nota;
+
+        do
+        {
+            Console.WriteLine(mensaje);
+            nota = Convert.ToDouble(Console.ReadLine());
+
+        } while (nota < 0 || nota > 20);
+
+        return nota;
+    }
+
+    static bool EsAprobado(double nota)
+    {
+        return nota >= 11;
+    }
+
+    static string ClasificarNota(double nota)
+    {
+        if (nota >= 18)
+        {
+            return "Excelente";
+        }
+        else if (nota >= 14)
+        {
+            return "Bueno";
+        }
+        else if (nota >= 11)
+        {
+            return "Aprobado";
+        }
+        else
+        {
+            return "Desaprobado";
+        }
+    }
+
+    static double CalcularPromedio(double suma, int n)
+    {
+        return suma / n;
+    }
+
+    static void MostrarEstadisticas(double prom, double mayor, double menor, int aprobados, int total)
+    {
+        Console.WriteLine("Promedio: " + prom);
+        Console.WriteLine("Mayor: " + mayor);
+        Console.WriteLine("Menor: " + menor);
+        Console.WriteLine("Aprobados: " + aprobados);
+        Console.WriteLine("Total: " + total);
+    }
+}
---
+++ PC_Python/PC7_Mostrar_Linea.py
@@ -0,0 +1,8 @@
+def MostrarLinea(caracter, n):
+
+    for i in range(n):
+        print(caracter, end="")
+
+MostrarLinea("*", 10)
+
+input()
